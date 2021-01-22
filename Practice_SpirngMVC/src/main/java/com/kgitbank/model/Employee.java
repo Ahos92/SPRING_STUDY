95d1ab6f385c8f00a8dcda080dc20e784f6b02ba
Ahos92 <ahosism@gmail.com>
2021-01-22 16:19:57 +0900

    1 / 22 spring start

---
+++ Practice_SpirngMVC/src/main/java/com/kgitbank/model/Employee.java
@@ -0,0 +1,14 @@
+package com.kgitbank.model;
+
+import lombok.Data;
+
+// 데이터를 담기위한 클래스 (데이터의 형태를 정의한 코드)
+@Data
+public class Employee {
+
+	private String employee_name;
+	private String first_name;
+	private String last_name;
+
+}
+
